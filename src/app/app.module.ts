918d4eb65e5d5b53c622ff6c7044d634d402c41c
Roger Adelmann <roger.adelmann@bnymellon.com>
2018-04-02 13:52:26 -0500

    Single Page Application Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component'; 
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -45,7 +46,7 @@ import { environment } from '../environments/environment';
     FlexLayoutModule,
 	AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService, PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,11 +1,13 @@
 import { Comment } from './comment'; /* Class for comment data types */
 
 export class Dish {
+	id: number;
     name: string;
     image: string;
     category: string;
     label: string;
     price: string;
+	featured: boolean;
     description: string;
 	comments: Comment[];
 }
